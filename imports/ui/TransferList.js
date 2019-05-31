0788be7d302756b2d7aa162fd188d67fce27f52f
nelsondude <alexn1336@gmail.com>
2019-05-30 20:57:36 -0700

    reorderable and transferable list

--- imports/ui/TransferList.js
+++ imports/ui/TransferList.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
@@ -12,19 +9,7 @@ import {withTracker} from "meteor/react-meteor-data";
 import {Questions} from "../api/links";
 import {SortableContainer, SortableElement} from 'react-sortable-hoc';
 import arrayMove from 'array-move';
-
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     margin: 'auto',
-//   },
-//   paper: {
-//     width: 200,
-//     overflow: 'auto',
-//   },
-//   button: {
-//     margin: theme.spacing(0.5, 0),
-//   },
-// }));
+import './TransferList.css';
 
 function not(a, b) {
   return a.filter(value => b.indexOf(value) === -1);
@@ -34,19 +19,24 @@ function intersection(a, b) {
   return a.filter(value => b.indexOf(value) !== -1);
 }
 
-const SortableItem = SortableElement(({value}) =>
-  <ListItem key={value} role={undefined} button onClick={() => this.handleToggle(value)}>
+const SortableItem = SortableElement(({value, checked, toggle}) =>
+  <li>
     <ListItemIcon>
-      <Checkbox checked={false} tabIndex={-1} disableRipple/>
+      <Checkbox checked={checked.indexOf(value) !== -1} tabIndex={-1} disableRipple
+                onClick={() => toggle(value)}/>
     </ListItemIcon>
-    <ListItemText primary={value.text}/>
-  </ListItem>);
+    <span className={'itemtext'}>
 
-const SortableList = SortableContainer(({items}) => {
+    {value.text}
+    </span>
+  </li>
+);
+
+const SortableList = SortableContainer(({items, checked, toggle}) => {
   return (
     <List dense>
       {items.map((value, index) => (
-        <SortableItem key={`item-${index}`} index={index} value={value} />
+        <SortableItem key={`item-${index}`} index={index} value={value} checked={checked} toggle={toggle}/>
       ))}
     </List>
   );
@@ -54,7 +44,7 @@ const SortableList = SortableContainer(({items}) => {
 
 class TransferList extends React.Component {
   state = {
-    left: [0, 1, 2, 3, 4, 5],
+    left: [],
     right: [],
     checked: []
   };
@@ -87,7 +77,6 @@ class TransferList extends React.Component {
   handleCheckedRight = () => {
     const {left, right, checked} = this.state;
     const leftChecked = intersection(checked, left);
-    const rightChecked = intersection(checked, right);
     this.setState({
       right: right.concat(leftChecked),
       left: not(left, leftChecked),
@@ -97,7 +86,6 @@ class TransferList extends React.Component {
 
   handleCheckedLeft = () => {
     const {left, right, checked} = this.state;
-    const leftChecked = intersection(checked, left);
     const rightChecked = intersection(checked, right);
 
     this.setState({
@@ -115,39 +103,36 @@ class TransferList extends React.Component {
     })
   };
 
-  onSortEnd = ({oldIndex, newIndex}) => {
-    this.setState(({items}) => ({
-      right: arrayMove(items, oldIndex, newIndex),
-    }));
-  };
+  leftList = (items) => (
+    <Paper style={{padding: '10px'}}>
+      <h1>My Questions</h1>
+      <SortableList items={items} onSortEnd={({oldIndex, newIndex}) => {
+        this.setState(({left}) => ({
+          left: arrayMove(left, oldIndex, newIndex),
+        }));
+      }} checked={this.state.checked} toggle={this.handleToggle}/>
+    </Paper>
+  );
 
-  customList = (items, title="") => (
-    <Paper style={{width: '200px'}}>
-      <h1>{title}</h1>
-      {/*<SortableList items={items} onSortEnd={this.onSortEnd} />*/}
-      <List dense>
-        {items.map(value => (
-          <ListItem key={value} role={undefined}>
-            <ListItemIcon>
-              <Checkbox checked={this.state.checked.indexOf(value) !== -1} tabIndex={-1} disableRipple onClick={()=>this.handleToggle(value)}/>
-            </ListItemIcon>
-            <ListItemText primary={value.text} />
-          </ListItem>
-        ))}
-        <ListItem />
-      </List>
+  rightList = (items) => (
+    <Paper style={{padding: '10px'}}>
+      <h1>Questions in Game</h1>
+      <SortableList items={items} onSortEnd={({oldIndex, newIndex}) => {
+        this.setState(({right}) => ({
+          right: arrayMove(right, oldIndex, newIndex),
+        }));
+      }} checked={this.state.checked} toggle={this.handleToggle}/>
     </Paper>
   );
 
   render() {
-    // const classes = useStyles();
     const {checked, left, right} = this.state;
     const leftChecked = intersection(checked, left);
     const rightChecked = intersection(checked, right);
 
     return (
-      <Grid container spacing={2} justify="center" alignItems="center" className={'TransferList'}>
-        <Grid item>{this.customList(left, 'My Questions')}</Grid>
+      <Grid container spacing={2} justify="space-evenly" className={'TransferList'}>
+        <Grid item style={{flexGrow: 1}}>{this.leftList(left)}</Grid>
         <Grid item>
           <Grid container direction="column" alignItems="center">
             <Button
@@ -188,7 +173,7 @@ class TransferList extends React.Component {
             </Button>
           </Grid>
         </Grid>
-        <Grid item>{this.customList(right, "Questions in Game")}</Grid>
+        <Grid item style={{flexGrow: 1}}>{this.rightList(right)}</Grid>
       </Grid>
     )
   }
