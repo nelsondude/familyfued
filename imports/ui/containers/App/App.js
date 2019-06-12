f2bf15dd6e6eb4870e03082d8c9d824d448c9b9b
nelsondude <alexn1336@gmail.com>
2019-06-11 17:34:02 -0700

    Only show buzzer on mobile devices

--- imports/ui/containers/App/App.js
+++ imports/ui/containers/App/App.js
@@ -6,7 +6,7 @@ import NewGameComponent from "../NewGame";
 import GamesComponent from '../Home';
 import RegularPlay from "../RegularPlay/RegularPlay";
 import FastMoney from "../FastMoney/FastMoney";
-
+import {isMobile} from 'react-device-detect';
 import './App.css';
 import Navbar from "../../components/Navbar";
 import Buzzer from "../Buzzer/Buzzer";
@@ -17,23 +17,31 @@ const styles = {
     minHeight: '400px',
     padding: '100px',
     marginTop: '50px'
+  },
+  mobile: {
+    padding: '20px'
   }
 };
 
 const EverythingButBuzzer = (props) => (
-  props.currentUser ?
-    <Switch>
-      <Route path="/" exact component={GamesComponent}/>
-      <Route path="/games/new" component={NewGameComponent}/>
-      <Route path="/games/:game_id/regular/:question_num" component={RegularPlay}/>
-      <Route path="/games/:game_id/fast/:round_num" component={FastMoney}/>
-    </Switch> :
+  !isMobile ?
+    props.currentUser ?
+      <Switch>
+        <Route path="/" exact component={GamesComponent}/>
+        <Route path="/games/new" component={NewGameComponent}/>
+        <Route path="/games/:game_id/regular/:question_num" component={RegularPlay}/>
+        <Route path="/games/:game_id/fast/:round_num" component={FastMoney}/>
+      </Switch> :
 
-    <Paper style={styles.paper}>
-      <h1>Welcome to the Family Fued Web App!</h1>
-      <h2>Login or Register to Play!</h2>
+      <Paper style={styles.paper}>
+        <h1>Welcome to the Family Fued Web App!</h1>
+        <h2>Login or Register to Play!</h2>
+      </Paper>
+    :
+    <Paper style={styles.mobile}>
+      <h2>This page is not available on a mobile device. Use a desktop to continue!</h2>
+      <h5>Only the buzzer is available on mobile devices.</h5>
     </Paper>
-
 );
 
 
